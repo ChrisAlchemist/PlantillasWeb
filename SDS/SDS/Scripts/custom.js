9217ecdd2c96bae7fb8fa643cf7e5174414b4320
Cristian Pérez García <cristian.perez@cmv.mx>
2020-03-05 17:05:30 -0600

    Se actualiza modal de portafolio.

--- SDS/SDS/Scripts/custom.js
+++ SDS/SDS/Scripts/custom.js
@@ -1,4 +1,16 @@
 (function ($) {
+
+    $(".slideshow-img:gt(0)").hide();
+
+    setInterval(function () {
+        $('.slideshow-img:first')
+            .fadeOut(1000)
+            .next()
+            .fadeIn(1000)
+            .end()
+            .appendTo('#slideshow');
+    }, 3000);
+
     
     // Init Wow
     wow = new WOW( {
@@ -67,20 +79,34 @@ function mostrarDescripcion(numPortafolio) {
 
     switch (numPortafolio) {
         case 1:
-            $("#imagenesPortafolio").attr("src", "/img/work/logicZotz/logicZotz4.jpg");
+            $("#titulo-portafolio").text("logicZotz");
+            
             $("#descripcionPortafolio").text("Video juego desarrollado en el lenguaje java, el cual tiene el objetivo de enseñar las bases de un leguaje de programación a niños de primaria y secundaria de entre 7 y 14 años. Esto claro en compañia del personaje favorito de la famosa serie de anime Dragon Ball.");
+            $("#imagenesPortafolio1").attr("src", "/img/work/logicZotz/logicZotz4.jpg");
+            $("#imagenesPortafolio2").attr("src", "/img/work/logicZotz/logicZotz3.jpg");
+            $("#imagenesPortafolio3").attr("src", "/img/work/logicZotz/logicZotz2.jpg");
             break;
         case 2:
-            $("#imagenesPortafolio").attr("src", "/img/work/cpr_soluciones/cpr_soluciones9.jpg");
+            $("#titulo-portafolio").text("CPR Soluciones");
+            
             $("#descripcionPortafolio").text("Prototipo web desarrollado en ReactJS y exclusivamente para la empresa deipi, el cual tiene como objetivo ayudar a la administracion y visualizacion de los productos de diferentes categotias y su vez brindar una mejor publicidad de ellos.");
+            $("#imagenesPortafolio1").attr("src", "/img/work/cpr_soluciones/cpr_soluciones9.jpg");
+            $("#imagenesPortafolio2").attr("src", "/img/work/cpr_soluciones/cpr_soluciones10.jpg");
+            $("#imagenesPortafolio3").attr("src", "/img/work/cpr_soluciones/cpr_soluciones7.jpg");
             break;
         case 3:
-            $("#imagenesPortafolio").attr("src", "/img/work/tgz/tgz4.jpg");
+            $("#titulo-portafolio").text("The Gamer Zone");            
             $("#descripcionPortafolio").text("Sitio web desarrollado para un club de gamer, en el cual se pueden enterar de lo ultimo del momento en videojuegos, dar opinion sobre algun videojuego y promocionar algun torneo o evento relacionado con esta area.");
+            $("#imagenesPortafolio1").attr("src", "/img/work/tgz/tgz4.jpg");
+            $("#imagenesPortafolio2").attr("src", "/img/work/tgz/tgz3.jpg");
+            $("#imagenesPortafolio3").attr("src", "/img/work/tgz/tgz2.jpg");
             break;
         case 4:
-            $("#imagenesPortafolio").attr("src", "/img/work/toz/toz4.jpg");
+            $("#titulo-portafolio").text("The Otaku Zone");            
             $("#descripcionPortafolio").text("Sitio web desarrollado para un club de anime, en el cual se pueden enterar de lo ultimo del momento en el mundo del anime, dar opinion sobre su distintas series de este genero, compartir sitios o articulos sobre anime y ver los ultimos capitulos de las series en emisión.");
+            $("#imagenesPortafolio1").attr("src", "/img/work/toz/toz4.jpg");
+            $("#imagenesPortafolio2").attr("src", "/img/work/toz/toz3.jpg");
+            $("#imagenesPortafolio3").attr("src", "/img/work/toz/toz2.jpg");
             break;
 
     }
